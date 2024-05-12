b8d695e88343ed9981345fd7afcf7e79f5b92750
FaizaTasneeem <faizat9696@gmail.com>
2024-05-12 22:46:47 +0600

    implemented sorting options by date and priority

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,30 +3,56 @@ import '../styles/Sidebar.css'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-function Sidebar({curList}) {
-  const [date, setDate] = useState(new Date());
+function Sidebar({curList, funcToSort}) {
+    const [date, setDate] = useState(new Date());
+    const [sortedCurList, setSortedCurList] = useState([]);
+    const priorityOrder = ['high', 'medium', 'low'];
 
-  const onChange = (newDate) => {
-    setDate(newDate);
-  };
+    function sortByDate() {
+        curList.sort((a, b) => {
+            const dateA = a.date ? new Date(a.date) : new Date('2075-01-01');
+            const dateB = b.date ? new Date(b.date) : new Date('2075-01-01');
+            return dateA - dateB;
+        })
+        setSortedCurList(curList);
+    }
 
-  useEffect(() => {
-    console.log(date);
-}, [date]);
+    function sortByPriority() {
+        curList.sort((a, b) => {
+            const priorityA = a.priority ? priorityOrder.indexOf(a.priority) : Number.MAX_SAFE_INTEGER;
+            const priorityB = b.priority ? priorityOrder.indexOf(b.priority) : Number.MAX_SAFE_INTEGER;
+            return priorityA - priorityB;
+        })
+        setSortedCurList(curList);
+    }
 
-  return (
-    <div className="sidebar-container">
-        <div style={{paddingTop:'15%'}}>Select a date to view tasks.</div>
-        <Calendar className="custom-calendar"
-            onChange={onChange}
-            value={date}
-        />
+    const onChange = (newDate) => {
+        setDate(newDate);
+    };
 
-        <div style={{paddingTop:'15%'}}>Sort By: </div>
-        <button className='sidebar-btns' style={{paddingTop:'15px', paddingBottom:'25px'}} >Date</button>
-        <button className='sidebar-btns' style={{paddingTop:'15px', paddingBottom:'25px'}} >Priority</button>
-    </div>
-  );
+   
+    useEffect(() => {
+        console.log(date);
+    }, [date]);
+
+    useEffect(() => {
+        funcToSort(sortedCurList);
+    }, [sortedCurList]);
+
+
+    return (
+        <div className="sidebar-container">
+            <div style={{paddingTop:'15%'}}>Select a date to view tasks.</div>
+            <Calendar className="custom-calendar"
+                onChange={onChange}
+                value={date}
+            />
+
+            <div style={{paddingTop:'15%'}}>Sort By: </div>
+            <button className='sidebar-btns' style={{paddingTop:'15px', paddingBottom:'25px'}} onClick={sortByDate}>Date</button>
+            <button className='sidebar-btns' style={{paddingTop:'15px', paddingBottom:'25px'}} onClick={sortByPriority}>Priority</button>
+        </div>
+    );
 };
 
 export default Sidebar;
